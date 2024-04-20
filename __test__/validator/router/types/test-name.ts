08236926668c9480f4129d6b6cc4e8f2bf50ed1a
Daniel-k-dev <juyeon10120@naver.com>
2024-04-21 03:47:56 +0900

    feat(validator): validator 테스트 설명에 사용될 상수값 추가

---
+++ __test__/validator/router/types/test-name.ts
@@ -0,0 +1,9 @@
+export enum TestName {
+  PASS = "통과",
+  FAIL = "실패",
+  VALID_REQUSET = "유효한 요청",
+  INVALID_REQUSET = "유효하지 않은 요청",
+  MISS_TYPE = "타입이 알맞지 않음",
+  NOT_EXIST_ATTRIBUTE = "속성이 누락됨",
+  EMPTY_ATTRIBUTE = "값이 비어있음"
+}
