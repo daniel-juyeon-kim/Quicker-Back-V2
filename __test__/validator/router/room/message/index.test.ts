64ef03074bef7989e3a46d06751fde1fd2aa2b1d
Daniel-k-dev <juyeon10120@naver.com>
2024-04-21 02:28:28 +0900

    feat(validator): /room/message의 검증 스키마 생성

---
+++ __test__/validator/router/room/message/index.test.ts
@@ -0,0 +1,62 @@
+import { NextFunction, Request, Response } from "express";
+import { Types, message, validate } from "../../../../../validator";
+import { getMethodSchema } from "../../../../../validator/schema/routes/room/message";
+import { TestName } from "../../types/test-name";
+
+let req: Partial<Request>;
+let res: Partial<Response>;
+let next: NextFunction;
+
+beforeEach(() => {
+  req = {};
+  res = {};
+  next = jest.fn();
+});
+
+describe("GET: /room/message", () => {
+  const testTarget = validate(getMethodSchema, ["query"]);
+
+  describe(TestName.VALID_REQUSET, () => {
+    test(TestName.PASS, async () => {
+      req.query = {
+        orderNum: "3",
+      };
+
+      await testTarget(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith();
+    });
+  });
+
+  describe(TestName.INVALID_REQUSET, () => {
+    test(TestName.NOT_EXIST_ATTRIBUTE, async () => {
+      req.query = {};
+
+      await testTarget(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith({
+        location: "query",
+        msg: message.notExist,
+        path: "orderNum",
+        type: "field",
+        value: undefined,
+      });
+    });
+
+    test(TestName.MISS_TYPE, async () => {
+      req.query = {
+        orderNum: "문자열",
+      };
+
+      await testTarget(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith({
+        location: "query",
+        msg: message.mustBe(Types.INT),
+        path: "orderNum",
+        type: "field",
+        value: "문자열",
+      });
+    });
+  });
+});
