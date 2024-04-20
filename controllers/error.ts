bbc985a2bb2aa42a4c243d14fd0ad20f2aa18b0f
Daniel-k-dev <juyeon10120@naver.com>
2024-04-20 21:42:44 +0900

    feat(errorHandler): express-validator에 대한 에러 처리와 기타 예외 핸들링이 가능하도록 수정

--- controllers/error.ts
+++ controllers/error.ts
@@ -1,17 +1,21 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from "express";
+import { HTTPErrorResponse } from "../service/http-response";
 import { slackBot } from "../service";
-import { HTTPError } from "../types/http-error";
+import { FieldValidationError, Result } from "express-validator";
 
-export class ErrorController {
-  handler(err: HTTPError, req: Request, res: Response, next: NextFunction) {
-    if (err.status === 404) {
-      res.send({ error_message: err.message });
-      next();
-    } else {
-      console.error(err);
-      slackBot.sendMessage(err);
-      res.json({ errorMessage: err.message });
-      next();
+export const errorController: ErrorRequestHandler = (err, _, res, __) => { 
+    if (err instanceof HTTPErrorResponse) { // 서비스 계층에서 생성된 에러 확인
+      return res.send(err);
     }
-  }
+    else if (isExpressValidationError(err)) { // express-validator 에러 확인, 유효성 검사에 실패한 요청
+      return res.send(new HTTPErrorResponse(400, err)); 
+    }
+    // 기타 에러 처리
+    slackBot.sendMessage(err);
+    console.log(err);
+    res.send(new HTTPErrorResponse(500, err.message));  
+};
+
+const isExpressValidationError = (err: any | Result<FieldValidationError>) => {
+  return err.type === 'field' && err.path && err.location
 }
