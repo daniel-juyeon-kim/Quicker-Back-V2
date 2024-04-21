12626c9f6443d5c58f4a7ab439d3033408c74a69
Daniel-k-dev <juyeon10120@naver.com>
2024-04-22 03:37:08 +0900

    refactor(validator): custom-validator.ts 내부코드 리팩토링

--- validator/util/custom-validator.ts
+++ validator/util/custom-validator.ts
@@ -1,9 +1,12 @@
 import { message } from "../";
 import { Types } from "../";
+import { checkNotNumber } from "../../service/checker";
 
 export const intArrayValidator = (arg: string) => {
-  arg.split(",").forEach((value) => {
-    if (Number.isNaN(parseInt(value))) {
+  arg.split(",").forEach((splitArg) => {
+    try {
+      checkNotNumber(splitArg)
+    } catch (e) {
       throw new Error(message.mustBe(Types.INT_ARRAY))
     }
   });
