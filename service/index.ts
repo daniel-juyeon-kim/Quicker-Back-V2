df18a4518aa8acbe2955f6ac0a381bb03f2f5913
Daniel-k-dev <juyeon10120@naver.com>
2024-08-28 21:47:44 +0900

    refactor(key-validator): key-validator를 사용하는 부분 삭제

--- service/index.ts
+++ service/index.ts
@@ -1,22 +1,16 @@
-import config from "../config";
-
+import { config } from "../config";
 import { BlockChain } from "./blockchain";
 import { Crypto } from "./cryto";
 import { DateFomater } from "./dateFormat";
-import { KeyChecker } from "./key-validator";
 import { NHNAPI } from "./nhn-api";
 import { SlackBot } from "./slack/slack-bot";
 import { TmapApi } from "./tmap";
 
-const keyChecker = new KeyChecker();
-const nhn = keyChecker.checkObject(config.nhn);
-const klaytn = keyChecker.checkObject(config.klaytn);
-
 const tmapApi = new TmapApi();
 const cryptoInstance = new Crypto();
 const dateFormater = new DateFomater();
-const nhnApi = new NHNAPI(nhn);
-const slackBot = new SlackBot(process.env.SLACK_BOT_TOKEN, process.env.SLACK_BOT_CHANNEL_ID);
-const blockChain = new BlockChain(klaytn.BAOBAB_PROVIDER);
+const nhnApi = new NHNAPI(config.nhnApi);
+const slackBot = new SlackBot(config.slackbot.SLACK_BOT_TOKEN, config.slackbot.SLACK_BOT_CHANNEL_ID);
+const blockChain = new BlockChain(config.klaytn.baobobProvider);
 
-export { tmapApi, nhnApi, slackBot, cryptoInstance, dateFormater, blockChain };
+export { blockChain, cryptoInstance, dateFormater, nhnApi, slackBot, tmapApi };
