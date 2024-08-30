b30284a600f94afae79f73b4b5ac6d0e38770354
Daniel-k-dev <juyeon10120@naver.com>
2024-08-30 14:33:21 +0900

    refactor(middleware): 더 이상 사용하지 않는 미들웨어 삭제
    - custom-morgan 과 관련된 코드 삭제

--- service/index.ts
+++ service/index.ts
@@ -1,16 +1,14 @@
 import { config } from "../config";
-import { Klaytn } from "./blockchain";
+import { Klaytn } from "./blockchain/blockchain";
 import { Crypto } from "./cryto";
-import { DateFomater } from "./dateFormat";
-import { NHNAPI } from "./nhn-api";
+import { NHNAPI } from "./message-sender/nhn-api";
 import { SlackBot } from "./slack/slack-bot";
 import { TmapApi } from "./tmap";
 
 const tmapApi = new TmapApi();
 const cryptoInstance = new Crypto();
-const dateFormater = new DateFomater();
 const nhnApi = new NHNAPI(config.nhnApi);
 const slackBot = new SlackBot(config.slackbot);
 const blockChain = new Klaytn(config.klaytn.baobobProvider);
 
-export { blockChain, cryptoInstance, dateFormater, nhnApi, slackBot, tmapApi };
+export { blockChain, cryptoInstance, nhnApi, slackBot, tmapApi };
