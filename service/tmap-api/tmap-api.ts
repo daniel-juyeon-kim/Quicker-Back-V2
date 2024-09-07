69ab7055632bca205626a4ce8069296896086e7f
Daniel-k-dev <juyeon10120@naver.com>
2024-09-08 03:28:57 +0900

    refactor(service): tmap-api 리팩토링
    - 비동기 에러 처리 추가
    - 요청에 필요한 타입 추가
      - RequestBody
      - ResponseBody
      - ErrorResponseBody

--- service/tmap-api/tmap-api.ts
+++ service/tmap-api/tmap-api.ts
@@ -1,16 +1,18 @@
+import fetch from "node-fetch";
 import { Location } from "../../maria/commands/location";
-import { isFulfilled, isUndefined } from "../../util";
-import { Env, validateEnvValue } from "../../util/env";
+import { isFulfilled, isNull, validateResponse } from "../../util";
+import { validateEnvValue } from "../../util/env";
+import { EnvConfig } from "../../util/env/types";
 import { ErrorMessage, ErrorMessageBot } from "../slack";
-import { RequestRouteDistanceBody, RouteDistance } from "./types";
+import { ErrorResponseBody, RequestBody, ResponseBody } from "./types";
 
 export class TmapApi {
-  private readonly requestApiUrl = `https://apis.openapi.sk.com/tmap/routes?version=1&format=json&appKey=`;
+  private readonly REQUEST_API_URL = `https://apis.openapi.sk.com/tmap/routes?version=1&format=json&appKey=`;
   private readonly appKey: string;
   private readonly slackbot: ErrorMessageBot;
   private readonly KM = 1000;
 
-  constructor(appKey: Env, slackbot: ErrorMessageBot) {
+  constructor(appKey: EnvConfig["tmapApiKey"], slackbot: ErrorMessageBot) {
     validateEnvValue("appKey", appKey);
 
     this.appKey = appKey;
@@ -20,6 +22,7 @@ export class TmapApi {
   public async requestRouteDistances(locations: Location[]) {
     const promises = locations.map(async (location) => {
       const requestBody = {
+        totalValue: 2,
         startX: location.Departure.X.toString(),
         startY: location.Departure.Y.toString(),
         endX: location.Destination.X.toString(),
@@ -28,27 +31,31 @@ export class TmapApi {
 
       const distance = await this.requestRouteDistance(requestBody);
 
-      return { orderId: location.id, km: distance };
+      return isNull(distance) ? null : { orderId: location.id, km: distance };
     });
 
     return (await Promise.allSettled(promises))
-      .filter(<T>(result: PromiseSettledResult<T>): result is PromiseFulfilledResult<T> => isFulfilled(result))
-      .map((result) => result.value)
-      .filter((result): result is { orderId: number; km: number } => !isUndefined(result.km));
+      .filter(isFulfilled)
+      .map((distance) => distance.value)
+      .filter((distance) => !isNull(distance));
   }
 
-  private async requestRouteDistance(body: RequestRouteDistanceBody) {
+  private async requestRouteDistance(body: RequestBody) {
     try {
-      const fetchedData = await fetch(this.requestApiUrl + this.appKey, {
+      const response = await fetch(this.REQUEST_API_URL + this.appKey, {
         method: "POST",
         body: JSON.stringify(body),
       });
 
-      const response = (await fetchedData.json()) as RouteDistance;
-      return response.features[0].properties.totalDistance / this.KM;
+      await validateResponse(response);
+
+      const responseBody = (await response.json()) as ResponseBody;
+      return responseBody.features[0].properties.totalDistance / this.KM;
     } catch (e) {
-      const error = e as Error;
-      this.slackbot.sendMessage(new ErrorMessage(error, new Date()));
+      const error = new Error((e as ErrorResponseBody).toString());
+      const errorMessage = new ErrorMessage(error, new Date());
+      this.slackbot.sendMessage(errorMessage);
+      return null;
     }
   }
 }
--- service/tmap-api/types/index.ts
+++ service/tmap-api/types/index.ts
@@ -1,14 +1,29 @@
-export interface RequestRouteDistanceBody {
+export interface RequestBody {
   startX: string;
   startY: string;
   endX: string;
   endY: string;
 }
 
-export interface RouteDistance {
-  features: {
-    properties: {
-      totalDistance: number;
-    };
-  }[];
+export interface ResponseBody {
+  type: "FeatureCollection";
+  features: [
+    {
+      type: "Feature";
+      properties: {
+        totalDistance: number;
+        totalTime: number;
+        totalFare: number;
+        taxiFare: number;
+      };
+    },
+  ];
 }
+export type ErrorResponseBody = {
+  error: {
+    id: string;
+    category: string;
+    code: string;
+    message: string;
+  };
+};
