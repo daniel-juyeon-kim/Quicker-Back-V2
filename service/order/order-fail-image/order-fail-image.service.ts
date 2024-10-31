c566ab01d8b91440afd9a14196178128cc2c1604
Daniel-k-dev <juyeon10120@naver.com>
2024-11-01 07:59:32 +0900

    feat(service): OrderFailImageService.createFailImage 추가

--- service/order/order-fail-image/order-fail-image.service.ts
+++ service/order/order-fail-image/order-fail-image.service.ts
@@ -1,9 +1,23 @@
+import { Request } from "express";
 import { FailDeliveryImageRepository } from "../../../database";
 
 export class OrderFailImageService {
   constructor(private readonly repository: FailDeliveryImageRepository) {}
 
   async findOrderFailImage(orderId: string) {
-    return this.repository.findFailDeliveryImageByOrderId(orderId);
+    return await this.repository.findFailDeliveryImageByOrderId(parseInt(orderId));
+  }
+
+  async createFailImage({
+    orderId,
+    reason,
+    file,
+  }: {
+    orderId: number;
+    reason: string;
+    file: Exclude<Request["file"], undefined>;
+  }) {
+    const bufferImage = file.buffer;
+    return await this.repository.createFailDeliveryImage({ orderId, reason, bufferImage });
   }
 }
