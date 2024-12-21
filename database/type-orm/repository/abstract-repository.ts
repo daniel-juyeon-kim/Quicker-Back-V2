446d6f8d62e375b9c0a32ea60d4bc3e48bd9a560
Daniel-k-dev <juyeon10120@naver.com>
2024-12-21 13:06:46 +0900

    refactor(database): UserRepository 저장 메서드 save -> insert로 변경

--- database/type-orm/repository/abstract-repository.ts
+++ database/type-orm/repository/abstract-repository.ts
@@ -2,8 +2,6 @@ import { isNull } from "../../../util";
 import { NotExistDataError } from "../util";
 
 export abstract class AbstractRepository {
-  protected readonly ERROR_MESSAGE_NOT_EXIST_DATA = "데이터가 존재하지 않습니다.";
-
   // eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
   protected validateNotNull<F extends Object, T>(findOption: F, data: T | null): asserts data is T {
     if (isNull(data)) {
